b3ea3e8adee6a917ea86fcf208d1582c781e9618
James Turk <james.p.turk@gmail.com>
2009-01-21 18:04:59 -0500

    pass all relevant params to can_vote_test

--- secretballot/views.py
+++ secretballot/views.py
@@ -16,15 +16,14 @@ def vote(request, content_type, object_id, vote, can_vote_test=None,
     # do the action
     if vote:
 
-        # if there is a can_vote_test func specified, test then 403 if needed
-        if can_vote_test and not can_vote_test(request, content_type, 
-                                                object_id, vote):
-            return HttpResponseForbidden("vote was forbidden")
-
         # 404 if object to be voted upon doesn't exist
         if content_type.model_class().objects.filter(pk=object_id).count() == 0:
             raise Http404
 
+        # if there is a can_vote_test func specified, test then 403 if needed
+        if can_vote_test:
+            if not can_vote_test(request, content_type, object_id, vote):
+                return HttpResponseForbidden("vote was forbidden")
 
         vobj,new = Vote.objects.get_or_create(content_type=content_type,
                                               object_id=object_id, token=token,
