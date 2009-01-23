e827680683aa3426d7f05a2a11d77526ae3eeb7b
James Turk <james.p.turk@gmail.com>
2009-01-23 18:46:38 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+
+long_description = open('README.rst').read()
+
+setup(
+    name='django-secretballot',
+    version="0.1",
+    package_dir={'secretballot': 'secretballot'},
+    packages=['secretballot'],
+    description='Django anonymous voting',
+    author='James Turk',
+    author_email='jturk@sunlightfoundation.com',
+    license='BSD License',
+    url='http://github.com/sunlightlabs/django-secretballot/',
+    long_description=long_description,
+    platforms=["any"],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Environment :: Web Environment',
+    ],
+)
