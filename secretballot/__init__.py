936ed70fa08582bfb2e45f018d66dc979982d40a
James Turk <james.p.turk@gmail.com>
2009-01-23 16:20:45 -0500

    removed dependency on functools

--- secretballot/__init__.py
+++ secretballot/__init__.py
@@ -1,9 +1,7 @@
 from secretballot.models import Vote
-from functools import partial
 
-def total_vote_limiter(request, content_type, object_id, vote, num):
-    return Vote.objects.filter(content_type=content_type, 
-                                token=request.secretballot_token).count() < num
-
-def limit_total_votes(num_votes):
-    return partial(total_vote_limiter, num=num_votes)
+def limit_total_votes(num):
+    def total_vote_limiter(request, content_type, object_id, vote):
+        return Vote.objects.filter(content_type=content_type, 
+                               token=request.secretballot_token).count() < num
+    return total_vote_limiter
