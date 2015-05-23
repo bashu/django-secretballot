627bcd3d3a4766c8b2f3ba967d5f3b590dffe647
James Turk <james.p.turk@gmail.com>
2015-05-23 10:59:54 -0400

    tests for the voting apparatus

--- tests/models.py
+++ tests/models.py
@@ -5,3 +5,18 @@ class Link(models.Model):
     url = models.URLField()
 
 secretballot.enable_voting_on(Link)
+
+
+# used for testing field renames
+class WeirdLink(models.Model):
+    url = models.URLField()
+
+secretballot.enable_voting_on(WeirdLink,
+                              votes_name='vs',
+                              upvotes_name='total_upvs',
+                              downvotes_name='total_downvs',
+                              total_name='v_total',
+                              add_vote_name='add_v',
+                              remove_vote_name='remove_v',
+                              )
+# TODO?: manager name & base_manager?
--- tests/tests.py
+++ tests/tests.py
@@ -4,7 +4,7 @@ from django.http import HttpRequest
 from secretballot.middleware import (SecretBallotMiddleware,
                                      SecretBallotIpMiddleware,
                                      SecretBallotIpUseragentMiddleware)
-from .models import Link
+from .models import Link, WeirdLink
 
 
 class MiddlewareTestCase(TestCase):
@@ -58,9 +58,9 @@ class MiddlewareTestCase(TestCase):
             mw.process_request(HttpRequest())
 
 
-class TestBasicVoting(TestCase):
+class TestVoting(TestCase):
 
-    def test_basic_voting(self):
+    def test_add_vote(self):
         l = Link.objects.create(url='https://google.com')
         assert Link.objects.get().vote_total == 0
 
@@ -72,3 +72,73 @@ class TestBasicVoting(TestCase):
 
         l.add_vote('1.2.3.6', -1)
         assert Link.objects.get().vote_total == 1
+
+    def test_up_and_down(self):
+        l = Link.objects.create(url='https://google.com')
+
+        l.add_vote('1.2.3.4', 1)
+        l.add_vote('1.2.3.6', -1)
+
+        l = Link.objects.get()
+        assert l.total_upvotes == 1
+        assert l.total_downvotes == 1
+        assert l.vote_total == 0
+
+    def test_remove_vote(self):
+        l = Link.objects.create(url='https://google.com')
+        assert Link.objects.get().vote_total == 0
+
+        l.add_vote('1.2.3.4', 1)
+        l.add_vote('1.2.3.5', 1)
+        assert Link.objects.get().vote_total == 2
+
+        l.remove_vote('1.2.3.5')
+        assert Link.objects.get().vote_total == 1
+
+    def test_from_token(self):
+        b = Link.objects.create(url='https://bing.com')
+        g = Link.objects.create(url='https://google.com')
+        y = Link.objects.create(url='https://yahoo.com')
+
+        # no vote on bing, +1 on google, -1 yahoo
+        g.add_vote('1.2.3.4', 1)
+        y.add_vote('1.2.3.4', -1)
+
+        sorted_links = Link.objects.from_token('1.2.3.4').order_by('url')
+        assert sorted_links[0].user_vote == None     # bing
+        assert sorted_links[1].user_vote == 1        # google
+        assert sorted_links[2].user_vote == -1       # yahoo
+
+
+    def test_from_request(self):
+        b = Link.objects.create(url='https://bing.com')
+        g = Link.objects.create(url='https://google.com')
+        y = Link.objects.create(url='https://yahoo.com')
+
+        # no vote on bing, +1 on google, -1 yahoo
+        g.add_vote('1.2.3.4', 1)
+        y.add_vote('1.2.3.4', -1)
+
+        # would be set by middleware
+        r = HttpRequest()
+        r.secretballot_token = '1.2.3.4'
+
+        sorted_links = Link.objects.from_request(r).order_by('url')
+        assert sorted_links[0].user_vote == None     # bing
+        assert sorted_links[1].user_vote == 1        # google
+        assert sorted_links[2].user_vote == -1       # yahoo
+
+
+class TestVotingWithRenamedFields(TestCase):
+
+    def test_everything_is_renamed(self):
+        # one big example to surface any issues in renaming fields
+        l = WeirdLink.objects.create(url='https://google.com')
+        l.add_v('1.2.3.4', 1)
+        l.add_v('1.2.3.5', -1)
+        l = WeirdLink.objects.get()
+        assert l.v_total == 0
+        assert l.total_upvs == 1
+        assert l.total_downvs == 1
+        assert l.vs.all()
+        assert l._secretballot_enabled is True
