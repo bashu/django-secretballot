b20dc827ca56656c96626bd9e0239d8fb7b99696
James Turk <james.p.turk@gmail.com>
2016-10-06 11:53:23 -0400

    bump to 0.5.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name='django-secretballot',
-    version="0.5.0",
+    version="0.5.1",
     packages=find_packages(),
     include_package_data=True,
     description='Django anonymous voting application',
