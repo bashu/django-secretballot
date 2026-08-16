f251c9492b45c49bc0dc22a4706909c6aafac29f
Basil Shubin <144783+bashu@users.noreply.github.com>
2026-08-16 18:27:48 +0600

    Bump version: 2.0.0 → 2.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2026, Basil Shubin'
 author = 'Basil Shubin'
 
 # The full version, including alpha/beta/rc tags
-release = "2.0.0"
+release = "2.0.1"
 
 
 # -- General configuration ---------------------------------------------------
